c9381575f41e3a044615c4a3afde443f6fadf7ff
rofoedu01 <skyline7002@gmail.com>
2024-09-02 23:57:25 -0400

    updated logging + log file location.

--- chocoFix/choco_fix.ps1
+++ chocoFix/choco_fix.ps1
@@ -1,4 +1,4 @@
-Start-Transcript -Path "$env:USERPROFILE\Documents\chocoFix$(Get-Date -UFormat "%Y-%m-%d@%Hh-%Mm-%Ss").log" 
+Start-Transcript -Path "Logs\chocoFix$(Get-Date -UFormat "%Y-%m-%d@%Hh-%Mm-%Ss").log" 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy unrestricted
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
